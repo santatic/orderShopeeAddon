f01f6c2e71a314a42dab9073f5479eb6d1ba8b6a
HuynhVuong87 <hv198197@gmail.com>
2018-06-16 14:48:48 +0700

    huynhnhob

--- app-content/shopee/frontend/single-product/controller.js
+++ app-content/shopee/frontend/single-product/controller.js
@@ -1,60 +1,76 @@
 app.controller("shopeeCtrl", ['$scope', 'moment',
-  function($scope, moment) {
-    $scope.item = {};
-    $scope.init = function(){
-        var regex = /\/.+i\.(\d+\.\d+)/g;
-        var matchs = regex.exec(window.location.href); 
-        //console.log(matchs)       ;
-        if(matchs !== null){
-            var shopid_itemid = matchs[1].split('.');
-            $scope.item.itemid = shopid_itemid[1];
-            $scope.item.shopid = shopid_itemid[0];
-        }        
-    }
-    $scope.init();
+    function ($scope, moment) {
+        $scope.item = {};
+        $scope.init = function () {
+            var regex = /\/.+i\.(\d+\.\d+)/g;
+            var matchs = regex.exec(window.location.href);
+            //console.log(matchs)       ;
+            if (matchs !== null) {
+                var shopid_itemid = matchs[1].split('.');
 
-    $scope.GLOBAL_FIRST_RESPONSE = {};
-    $scope.timeSubtract = function(){
-        if($scope.GLOBAL_FIRST_RESPONSE.hasOwnProperty($scope.item.itemid)){
-            return moment($scope.GLOBAL_FIRST_RESPONSE[$scope.item.itemid]['createdTime']).fromNow();    
+                $scope.item.itemid = shopid_itemid[1];
+                $scope.item.shopid = shopid_itemid[0];
+                console.log($scope.item.itemid);
+                console.log($scope.item.shopid);
+            }
         }
-        else
-            return 'loading';
-    }
-    
-    $scope.urlToPromise = function(url) {
-        return new Promise(function(resolve, reject) {
-            JSZipUtils.getBinaryContent(url, function (err, data) {
-                if(err) {
-                    reject(err);
-                } else {
-                    resolve(data);
-                }
+        $scope.init();
+
+        $scope.GLOBAL_FIRST_RESPONSE = {};
+        $scope.timeSubtract = function () {
+            if ($scope.GLOBAL_FIRST_RESPONSE.hasOwnProperty($scope.item.itemid)) {
+                return moment($scope.GLOBAL_FIRST_RESPONSE[$scope.item.itemid]['createdTime']).fromNow();
+            } else
+                return 'loading';
+        }
+
+        $scope.urlToPromise = function (url) {
+            return new Promise(function (resolve, reject) {
+                JSZipUtils.getBinaryContent(url, function (err, data) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(data);
+                    }
+                });
             });
-        });
-    }
-
-    $scope.download_images = function(){
-        var zip = new JSZip();
-        var folderName = '';
-
-        folderName = $scope.item.itemid;
-        if($scope.GLOBAL_FIRST_RESPONSE[$scope.item.itemid]['image_list'] !== null){
-
-            $scope.GLOBAL_FIRST_RESPONSE[$scope.item.itemid]['image_list'].forEach(function(url){
-                var filename = url.replace(/.*\//g, "");
-                zip.file(filename + '.jpg', $scope.urlToPromise('https://cf.shopee.vn/file/' + url), {binary:true});
-            });          
         }
-        else
-            new Noty({layout: 'center',type: 'success', text: 'Bạn phải chờ trang load xong đã'}).show();  
-               
-        zip.generateAsync({type:"blob"}).then(function (zipFile) {
-          saveAs(zipFile, folderName + '.zip');
-        });    
+        $('._19OC6A').each(function () {
+            var url = $(this).find('img').attr('src')
+            var filename = url.replace(/.*\//g, "");
+            console.log(filename);
+        })
 
-       
-        
+        $scope.download_images = function () {
+            var zip = new JSZip();
+            var folderName = '';
+
+            folderName = $scope.item.itemid;
+            // console.log($scope.GLOBAL_FIRST_RESPONSE[$scope.item.itemid]);
+            if ($('._19OC6A') !== 0) {
+
+                $('._19OC6A').each(function () {
+                    var url = $(this).find('img').attr('src')
+                    var filename = url.replace(/.*\//g, "");
+                    zip.file(filename + '.jpg', $scope.urlToPromise(url), {
+                        binary: true
+                    });
+                });
+            } else
+                new Noty({
+                    layout: 'center',
+                    type: 'success',
+                    text: 'Bạn phải chờ trang load xong đã'
+                }).show();
+
+            zip.generateAsync({
+                type: "blob"
+            }).then(function (zipFile) {
+                saveAs(zipFile, folderName + '.zip');
+            });
+
+
+
+        }
     }
-  }]
-);
+]);
