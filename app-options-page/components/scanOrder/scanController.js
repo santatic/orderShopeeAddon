5224c41a4809d993acda270ed4c5c94407165946
HuynhVuong87 <hv198197@gmail.com>
2018-06-16 21:41:19 +0700

    huynhnhob

--- app-options-page/components/scanOrder/scanController.js
+++ app-options-page/components/scanOrder/scanController.js
@@ -1,19 +1,33 @@
 app.controller("scan-controller", function ($scope, $routeParams) {
-    angular.element(document).ready(function() {
-        angular.element('#reader').html5_qrcode(function(data) { // start webcam
-              console.log(data); // print QR code content
-              document.getElementById('play-beep').play()
+  let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+      scanner.addListener('scan', function (content) {
+        console.log(content);
+        document.getElementById('play-beep').play()
             //   angular.element("#reader").html5_qrcode_stop(); // stop webcam 
               var saleUrl = chrome.extension.getURL("options.html#/orders/");
-              var win = window.open(saleUrl + data, '_blank');
+              var win = window.open(saleUrl + content, '_blank');
               win.focus()
-            },
-            function(error) {
-              console.log(error);
-            },
-            function(videoError) {
-              console.log(videoError);
-            }
-        );
-    });
+      });
+      Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+          scanner.start(cameras[0]);
+        } else {
+          console.error('No cameras found.');
+        }
+      }).catch(function (e) {
+        console.error(e);
+      });
+    // angular.element(document).ready(function() {
+    //     angular.element('#reader').html5_qrcode(function(data) { // start webcam
+    //           console.log(data); // print QR code content
+              
+    //         },
+    //         function(error) {
+    //           console.log(error);
+    //         },
+    //         function(videoError) {
+    //           console.log(videoError);
+    //         }
+    //     );
+    // });
 })
